70c833b5c6a42ab07bad791b721c17d0e6cb7945
RIM <navyfox8@gmail.com>
2017-12-24 09:31:45 +0600

    Loop

--- js/script.js
+++ js/script.js
@@ -2,12 +2,58 @@ var slideIndex=0;
 var isLeftToRight = 0;
 var startPosition = 0;
 var slidePosition = 1;
-var sliderList = document.getElementsByClassName("mySlides");
-var slides = document.getElementsByClassName("picture");
+var pos = 0;
+var slideWidth = 1000;
+var sliderList = document.querySelector(".mySlides");
+var slides = document.querySelectorAll(".picture");
 //showSlides(slideIndex);
 
 function plusSlides(n){
 	showSlides(n);
+}
+function scrollToPrev(n) {
+	pos--;
+	console.log(pos);
+	console.log(startPosition);
+	if (pos < 0) {
+		var children = sliderList.children;
+		sliderList.style.left = 0 + 'px';
+		startPosition = -1000;
+		console.log(sliderList.style.left);
+		//sliderList.style.left = -(pos + 2) * slideWidth + 'px';
+		sliderList.insertBefore(children[slides.length - 1], children[0]);
+		pos++;
+	}
+	requestAnimationFrame(function(){ //ожидаем следующего запланированного reflow/repain;
+		requestAnimationFrame(function(){
+			//предыдущий reflow рассчитал новый dom элемент
+			//можно делать анимацию.
+			showSlides(n);
+		})
+	});
+}
+
+function scrollToNext(n) {
+	console.log(pos);
+	pos++;
+	console.log(pos);
+	console.log(startPosition);
+	if (pos > slides.length -1) {
+		sliderList.style.left = "0px";
+		sliderList.style.left = -1000 + 'px';
+		startPosition = -1000;
+		var cloneElem = sliderList.children[0].cloneNode(true);
+		sliderList.appendChild(cloneElem);
+		sliderList.removeChild(sliderList.children[0]);
+		pos--;
+	}
+	requestAnimationFrame(function(){ //ожидаем следующего запланированного reflow/repain;
+		requestAnimationFrame(function(){
+			//предыдущий reflow рассчитал новый dom элемент
+			//можно делать анимацию.
+			showSlides(n);
+		})
+	});
 }
 
 function currentSlide(n){
@@ -32,14 +78,12 @@ function showSlides(n) {
 			timePassed = 1000;
 			clearInterval(timer);
 			if (isLeftToRight) {
-				slidePosition -= 1;
-				console.log(slidePosition);
+				console.log(startPosition);
 				startPosition = startPosition + timePassed;
 				train.style.left = startPosition + 'px';
 
 			} else {
-				slidePosition += 1;
-				console.log(slidePosition);
+				console.log(startPosition);
 				startPosition = startPosition - timePassed;
 				train.style.left = startPosition + 'px';
 
